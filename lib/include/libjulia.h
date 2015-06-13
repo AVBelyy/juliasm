868be75be09f23679764cd422acf077676ca4188
Anton Belyy <anton.belyy@gmail.com>
2015-06-14 01:42:03 +0300

    Refactored names

--- lib/include/libjulia.h
+++ lib/include/libjulia.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-Image * juliaGenerateImage(uint64_t w, uint64_t h, float a, float b, float scale);
+Image * juliaNewImage(uint64_t w, uint64_t h, float a, float b, float scale);
 void juliaGeneratePart(Image * out, uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2);
 
 #ifdef __cplusplus
