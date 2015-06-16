d239469ee4b08ab495e8794fe87c81bf50c38829
Anton Belyy <anton.belyy@gmail.com>
2015-06-16 03:32:23 +0300

    Corrected interface for handling negative parts generation

--- lib/include/libjulia.h
+++ lib/include/libjulia.h
@@ -14,7 +14,7 @@ struct JuliaPart {
 extern "C" {
 #endif
 
-void juliaGeneratePart(JuliaPart * info, void * out, uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2);
+void juliaGeneratePart(JuliaPart * info, void * out, int64_t x1, int64_t y1, int64_t x2, int64_t y2);
 
 #ifdef __cplusplus
 }
